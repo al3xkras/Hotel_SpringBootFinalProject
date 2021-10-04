4dff15532390ad1b1add49d5f9d7ebef3952270d
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-04 18:18:20 +0300

    Edit Main class & application.properties

--- src/main/java/ua/alexkras/hotel/HotelApplication.java
+++ src/main/java/ua/alexkras/hotel/HotelApplication.java
@@ -2,12 +2,37 @@ package ua.alexkras.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 
 @SpringBootApplication
-public class HotelApplication {
+@EnableWebMvc
+public class HotelApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
