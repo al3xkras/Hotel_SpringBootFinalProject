0da9cda14df87179e333dd2ecc46a2b93dd2ada6
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-05 13:26:53 +0300

    Edit Main page

--- src/main/java/ua/alexkras/hotel/HotelApplication.java
+++ src/main/java/ua/alexkras/hotel/HotelApplication.java
@@ -4,16 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 
 @SpringBootApplication
-@EnableWebMvc
 public class HotelApplication implements WebMvcConfigurer {
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/"
+	};
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
@@ -34,5 +39,13 @@ public class HotelApplication implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
 	}
 }
