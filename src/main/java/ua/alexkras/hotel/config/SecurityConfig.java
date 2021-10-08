e4786dd1cd1fe5fc24fa69edfc7e0b12d53ad7ba
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-08 20:13:03 +0300

    Configure Spring security, add admin & user personal area

--- src/main/java/ua/alexkras/hotel/config/SecurityConfig.java
+++ src/main/java/ua/alexkras/hotel/config/SecurityConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ua.alexkras.hotel.dao.UserDAO;
+import ua.alexkras.hotel.model.UserType;
 
 @Configuration
 @EnableWebSecurity
@@ -18,8 +18,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/personal_area/**")
+                    .hasAnyAuthority(UserType.ADMIN.name(),UserType.USER.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/personal_area")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout","POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/auth/login");
     }
 
     @Bean
--- src/main/java/ua/alexkras/hotel/controller/PagesController.java
+++ src/main/java/ua/alexkras/hotel/controller/PagesController.java
@@ -1,14 +1,33 @@
 package ua.alexkras.hotel.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ua.alexkras.hotel.model.UserType;
 
 @Controller
 public class PagesController {
 
     @RequestMapping("/")
+    @PreAuthorize("permitAll()")
     public String mainPage(){
         return "index";
     }
 
+    @GetMapping("/personal_area")
+    public String personalAreaPage(){
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (user.getAuthorities().contains(new SimpleGrantedAuthority(UserType.USER.name()))){
+            return "personal_area/user";
+        } else if (user.getAuthorities().contains(new SimpleGrantedAuthority(UserType.ADMIN.name()))){
+            return "personal_area/admin";
+        }
+
+        return "/index";
+    }
 
 }
