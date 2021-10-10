7341209d9f9fbf1c23935fff482e811ca1dfcc62
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-10 13:26:35 +0300

    Edit ApartmentClass enum

--- src/main/java/ua/alexkras/hotel/model/ApartmentClass.java
+++ src/main/java/ua/alexkras/hotel/model/ApartmentClass.java
@@ -1,7 +1,18 @@
 package ua.alexkras.hotel.model;
 
 public enum ApartmentClass {
-    Business,
-    Comfort,
-    Default,
+    ClassA("Class A"),
+    ClassB("Class B"),
+    ClassC("Class C"),
+    ClassD("Class D");
+
+    String apartmentName;
+
+    ApartmentClass(String apartmentName){
+        this.apartmentName=apartmentName;
+    }
+
+    public String getApartmentName() {
+        return apartmentName;
+    }
 }
