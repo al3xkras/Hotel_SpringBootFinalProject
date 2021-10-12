6789bd715b8cb36034ffc030249e8118ee63054d
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-12 16:06:10 +0300

    Add bootstrap & edit page's style

--- src/main/java/ua/alexkras/hotel/controller/PagesController.java
+++ src/main/java/ua/alexkras/hotel/controller/PagesController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ua.alexkras.hotel.dao.UserDAO;
 import ua.alexkras.hotel.entity.User;
 import ua.alexkras.hotel.model.UserType;
 import ua.alexkras.hotel.service.ReservationService;
